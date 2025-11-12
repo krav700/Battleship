f128b6e0c9c8f38196d1d04e9e4b9c27ca45af03
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-11-12 10:45:56 +0200

    feat: add tests for Ship()

--- mainPieces.test.js
+++ mainPieces.test.js
@@ -1,9 +1,18 @@
 import { Ship } from "./mainPieces.js";
 
 describe('Ship()', () => {
+    let ship1;
+    beforeEach(() => {
+        ship1 = Ship(3);
+    });
     test("Ship can be hit", () => {
-        const ship1 = Ship(3);
         ship1.hit();
         expect(ship1.hitTimes).toBe(1);
+    });
+    test("Ship can be sunk", () => {
+        ship1.hit();
+        ship1.hit();
+        ship1.hit();
+        expect(ship1.isSunk()).toBe(true);
     });
 });
