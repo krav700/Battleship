5b1fc24645cd8dba752ea2b623f826e0e40fa792
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-11-12 09:15:44 +0200

    fix: configure correctly babel and jest

--- mainPieces.test.js
+++ mainPieces.test.js
@@ -1,3 +1,9 @@
-describe('Ship()', () => {
+import { Ship } from "./mainPieces.js";
 
+describe('Ship()', () => {
+    test("Ship can be hit", () => {
+        const ship1 = Ship(3);
+        ship1.hit();
+        expect(ship1.hitTimes).toBe(1);
+    });
 });
