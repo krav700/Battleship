ccd4a402f0ef68c1af9589619129113280142e52
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-11-12 16:00:36 +0200

    fix: change the logic for tiles

--- mainPieces.js
+++ mainPieces.js
@@ -1,20 +1,84 @@
 export function Ship(length) {
     let hitTimes = 0;
-    let sunk = false;
 
     function hit() {
         hitTimes++;
-
     }
 
     function isSunk() {
         if (hitTimes == length) {
-            sunk = true;
-            return sunk;
+            return true;
         }
-        return sunk;
+        return false;
     }
 
-    return { length, get hitTimes() { return hitTimes; }, get sunk() { return sunk }, hit, isSunk};
+    return {
+        length,
+        get hitTimes() {
+            return hitTimes;
+        },
+        hit,
+        isSunk,
+    };
 }
 
+// Create an Object containing basicValue (the ones bellow) and shipValue if not a ship null
+// 0 - water
+// 1 - hit (miss)
+// 2 - ship (hidden)
+// 3 - ship (hit)
+
+function tileObject(type) {
+    let ship;
+
+    function setShip(tileShip) {
+        ship = tileShip;
+    }
+
+    return {type, get ship() { return ship; }, setShip};
+}
+
+export function Gameboard() {
+    let playerBoard = new Array(10).fill().map(() => new Array(10).fill().map(() => tileObject(0)));
+
+    //placeVertically
+    //placeHorizontally
+    //Idea: always presume you are placing the ships by holding them to the down/left side;
+    function placeShip(placeV, placeH, shipLength, direction = 'Horitontal') {
+        if (placeV < 0 || placeV > 9 || placeH < 0 || placeH > 9) {
+            console.log('Invalid Coordinates');
+            return;
+        }
+
+        const currentShip = Ship(shipLength);
+        if (direction == 'Vertical') {
+            while (placeV - shipLength + 1 < 0) {
+                placeV++;
+            }
+            for (let i = 0; i < shipLength; i++) {
+                playerBoard[placeV][placeH].type = 2;
+                playerBoard[placeV--][placeH].setShip(currentShip);
+            }
+        } else {
+            while ((placeH + shipLength - 1) > 9) {
+                placeH--;
+            }
+            for (let i = 0; i < shipLength; i++) {
+                playerBoard[placeV][placeH].type = 2;
+                playerBoard[placeV][placeH++].setShip(currentShip);
+            }
+        }
+    }
+
+    function receiveAttack() {
+
+    }
+
+    return {
+        get playerBoard() {
+            return playerBoard;
+        },
+        placeShip,
+        receiveAttack,
+    };
+}
