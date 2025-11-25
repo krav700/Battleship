e2cd417fd871991b26be2b495d764f76713ccdcd
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-11-25 10:00:10 +0200

    feat: move all files inside src and add explosion on hit

---
+++ src/scripts/mainPieces.js
@@ -0,0 +1,150 @@
+export function Ship(length) {
+    let hitTimes = 0;
+
+    function hit() {
+        hitTimes++;
+    }
+
+    function isSunk() {
+        if (hitTimes == length) {
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        length,
+        get hitTimes() {
+            return hitTimes;
+        },
+        hit,
+        isSunk,
+    };
+}
+
+// Create an Object containing basicValue (the ones bellow) and shipValue if not a ship null
+// 0 - water
+// 1 - hit (miss)
+// 2 - ship (hidden)
+// 3 - ship (hit)
+
+function tileObject(type) {
+    let ship;
+
+    function setShip(tileShip) {
+        ship = tileShip;
+    }
+
+    return {
+        type,
+        get ship() {
+            return ship;
+        },
+        setShip,
+    };
+}
+
+export function Gameboard() {
+    let playerBoard = new Array(10)
+        .fill()
+        .map(() => new Array(10).fill().map(() => tileObject(0)));
+    let sunkenShips = 0;
+    let loose = false;
+    //placeVertically
+    //placeHorizontally
+    //Idea: always presume you are placing the ships by holding them to the down/left side;
+    function placeShip(placeV, placeH, shipLength, direction = "Horitontal") {
+        if (placeV < 0 || placeV > 9 || placeH < 0 || placeH > 9) {
+            console.log("Invalid Coordinates");
+            return false;
+        }
+
+        const currentShip = Ship(shipLength);
+        if (direction == "Vertical") {
+            while (placeV - shipLength + 1 < 0) {
+                placeV++;
+            }
+            let tempPlaceV = placeV;
+            for (let i = 0; i < currentShip.length; i++) {
+                if (playerBoard[tempPlaceV--][placeH].type == 2) {
+                    console.log("There is already a ship there");
+                    return false;
+                }
+            }
+            for (let i = 0; i < shipLength; i++) {
+                playerBoard[placeV][placeH].type = 2;
+                playerBoard[placeV--][placeH].setShip(currentShip);
+            }
+        } else {
+            while (placeH + shipLength - 1 > 9) {
+                placeH--;
+            }
+            let tempPlaceH = placeH;
+            for (let i = 0; i < currentShip.length; i++) {
+                if (playerBoard[placeV][tempPlaceH++].type == 2) {
+                    console.log("There is already a ship there");
+                    return false;
+                }
+            }
+            for (let i = 0; i < shipLength; i++) {
+                playerBoard[placeV][placeH].type = 2;
+                playerBoard[placeV][placeH++].setShip(currentShip);
+            }
+        }
+        return true;
+    }
+
+    function receiveAttack(hitX, hitY) {
+        if (
+            playerBoard[hitX][hitY].type == 1 ||
+            playerBoard[hitX][hitY].type == 3
+        ) {
+            console.log("Already shot there");
+            return;
+        }
+
+        if (playerBoard[hitX][hitY].type == 2) {
+            console.log('type 2');
+            playerBoard[hitX][hitY].ship.hit();
+            playerBoard[hitX][hitY].type = 3;
+            if (playerBoard[hitX][hitY].ship.isSunk()) {
+                sunkenShips++;
+                console.log('SUNKEN')
+                console.log(sunkenShips);
+                if (sunkenShips == 5) {
+                    loose = true;
+                }
+            }
+        } else if (playerBoard[hitX][hitY].type == 0) {
+            playerBoard[hitX][hitY].type = 1;
+        }
+    }
+
+    return {
+        get playerBoard() {
+            return playerBoard;
+        },
+        placeShip,
+        receiveAttack,
+        get loose() {
+            return loose;
+        }
+    };
+}
+
+export function Player() {
+    let placedShips = 0;
+
+    function placedShip() {
+        placedShips++;
+    }
+
+    let playerBoard = Gameboard();
+
+    function resetPlayerBoard() {
+        playerBoard = Gameboard();
+    }
+
+
+    return { get playerBoard() { return playerBoard;}, resetPlayerBoard, get placedShips() { return placedShips;}, placedShip };
+}
