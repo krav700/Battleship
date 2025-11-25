66b64e2c563276c307e03c9ea0562c8fa635ba79
Stefan Ivanov <108619056+krav700@users.noreply.github.com>
2025-11-25 09:55:12 +0200

    feat: configure webpack files

---
+++ webpack/webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.resolve(__dirname, "../src/scripts/DOMInteraction.js"),
+    output: {
+        filename: "main.js",
+        path:path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../src/index.html"),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|jpg|jpeg|mp4)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
